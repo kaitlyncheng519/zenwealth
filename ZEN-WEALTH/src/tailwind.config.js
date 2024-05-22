7b804182ab0a8ba92f9713653ed2fdbb30c6824e
kaitlyncheng519 <144399760+kaitlyncheng519@users.noreply.github.com>
2024-05-22 00:58:22 -0400

    Add files via upload

---
+++ ZEN-WEALTH/src/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        indigo: {
+            DEFAULT: '#8C52FF',
+            '50': '#F5F3FF',
+            '100': '#EDE9FE',
+            '200': '#DDD6FE',
+            '300': '#C4B5FD',
+            '400': '#A78BFA',
+            '500': '#8C52FF', //my custom
+            '600': '#6366F1',
+            '700': '#4F46E5',
+            '800': '#4338CA',
+            '900': '#3730A3',
+        }
+      },
+    },
+  }
